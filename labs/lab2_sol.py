d288dc4552a56531060e23a97ba939b1eacf9f5e
Marco D'Almo <marcodalmo@MacBook-Pro-di-Marco.local>
2022-05-30 17:45:47 +0200

    Lab2 + figures from running the lab

--- labs/lab2_sol.py
+++ labs/lab2_sol.py
@@ -1,3 +1,4 @@
+import os
 import numpy
 import matplotlib
 import matplotlib.pyplot as plt
@@ -8,33 +9,102 @@ def mcol(v):
     return v.reshape((v.size, 1))
 
 def load(fname):
-    DList=[]
-    labelsList=[]
+    DList = []
+    labelsList = []
 
     # dictionary for labels
-    hlabels={
-        'iris-setosa': 0,
-        'iris-versicolor': 1,
-        'iris-virginica': 2
+    hLabels={
+        'Iris-setosa': 0,
+        'Iris-versicolor': 1,
+        'Iris-virginica': 2
         }
     
     # load function, builds attributes numpy array and labels one
     with open(fname) as f:
         for line in f:
             try:
-                attrs=line.split(',')[0:4]
-                attrs=mcol(numpy.array([float(i) for i in attrs]))
-                name = line.split(',')[-1].strip()
-                label=hlabels(name)
+                attrs = line.split(',')[0:4]
+                attrs = mcol(numpy.array([float(i) for i in attrs]))
+                name = line.split(',')[-1].strip('\n')
                 DList.append(attrs)
+                label = hLabels[name]
                 labelsList.append(label)
+
             except:
                 pass
-
+    
     return numpy.hstack(DList), numpy.array(labelsList, dtype=numpy.int32)
+
 
 def load2():
 
     # The dataset is already available in the sklearn library (pay attention that the library represents samples as row vectors, not column vectors - we need to transpose the data matrix)
     import sklearn.datasets
     return sklearn.datasets.load_iris()['data'].T, sklearn.datasets.load_iris()['target']
+
+def plot_hist(D, L):
+
+    D0 = D[:, L==0]
+    D1 = D[:, L==1]
+    D2 = D[:, L==2]
+
+    hFea = {
+        0: 'Sepal length',
+        1: 'Sepal width',
+        2: 'Petal length',
+        3: 'Petal width'
+        }
+
+    for dIdx in range(4):
+        plt.figure()
+        plt.xlabel(hFea[dIdx])
+        plt.hist(D0[dIdx, :], bins = 10, density = True, alpha = 0.4, label = 'Setosa')
+        plt.hist(D1[dIdx, :], bins = 10, density = True, alpha = 0.4, label = 'Versicolor')
+        plt.hist(D2[dIdx, :], bins = 10, density = True, alpha = 0.4, label = 'Virginica')
+        
+        plt.legend()
+        plt.tight_layout() # Use with non-default font size to keep axis label inside the figure
+        plt.savefig('labs/lab2_figures/hist_%d.pdf' % dIdx)
+    plt.show()
+
+def plot_scatter(D, L):
+    
+    D0 = D[:, L==0]
+    D1 = D[:, L==1]
+    D2 = D[:, L==2]
+
+    hFea = {
+        0: 'Sepal length',
+        1: 'Sepal width',
+        2: 'Petal length',
+        3: 'Petal width'
+        }
+
+    for dIdx1 in range(4):
+        for dIdx2 in range(4):
+            if dIdx1 == dIdx2:
+                continue
+            plt.figure()
+            plt.xlabel(hFea[dIdx1])
+            plt.ylabel(hFea[dIdx2])
+            plt.scatter(D0[dIdx1, :], D0[dIdx2, :], label = 'Setosa')
+            plt.scatter(D1[dIdx1, :], D1[dIdx2, :], label = 'Versicolor')
+            plt.scatter(D2[dIdx1, :], D2[dIdx2, :], label = 'Virginica')
+        
+            plt.legend()
+            plt.tight_layout() # Use with non-default font size to keep axis label inside the figure
+            plt.savefig('labs/lab2_figures/scatter_%d_%d.pdf' % (dIdx1, dIdx2))
+        plt.show()
+
+    
+if __name__ == '__main__':
+
+    # Change default font size - comment to use default values
+    plt.rc('font', size=16)
+    plt.rc('xtick', labelsize=16)
+    plt.rc('ytick', labelsize=16)
+
+    D, L = load('/Users/marcodalmo/Desktop/Poli/Magistrale/MacLearn/maclearn-git-repo/machine-learning-labs-and-projects/labs/iris.csv')
+    # D, L = load2()
+    plot_hist(D, L)
+    plot_scatter(D, L)
