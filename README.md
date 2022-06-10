db8cb250e44f436b75f6343119b587498db850a9
Marco D'Almo <marcodalmo@MacBook-Pro-di-Marco.local>
2022-06-10 20:37:08 +0200

    Lab 4 progress, still missing a part

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Lab 3 needs finishing on the Linear Discriminant analysis exercise (second part).
 
-Lab 4 just started, prepping the various functions that are supposed to to stuff.
+Lab 4 -- still finishing the part with the "real" data.
--- labs/lab4_sol.py
+++ labs/lab4_sol.py
@@ -1,25 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def createVariables():
+    # create variables to test the functions
+    X = np.linspace(-8, 12, 1000)
+    # make it a row matrix
+    x = vrow(X)
+    m = np.ones((1,1)) * 1.0
+    C = np.ones((1,1)) * 2.0
+
+    return x, m, C
+
+def vrow(m):
+     return m.reshape((1, m.size))
+
+def mcol(m):
+    return m.reshape((m.size, 1))
+
+def logpdf_1sample(values, Mu, C):
+
+    #computation of the logMultiVariateGaussian for each sample
+    P = np.linalg.inv(C)
+    logN = -0.5*values.shape[0]*np.log(2*np.pi)-0.5*np.linalg.slogdet(C)[1]-0.5*np.dot(np.dot((values-Mu).T, P), (values-Mu)) 
+
+    return logN.ravel()
+
+def logpdf_GAU_ND(X, mu, C):
+    Y = [logpdf_1sample(X[:, i:i+1], mu, C) for i in range(X.shape[1])]
+    return np.array(Y).ravel()
 
 
-#def multiVariateGaussian(x, Mu, C):
-
-
-#def checkResults():
-
-
-#def plot_Multivariate():
+def plot_Multivariate(MVG, X):
+    arrayx = np.array(X)
+    plt.figure()
+    plt.plot(arrayx.ravel(), np.exp(MVG))
+    plt.show()
 
 
 
-#if __name__ == '__main__': 
+if __name__ == '__main__': 
     
-    #x, Mu, C = createVariables()
-    #MVG = multiVariateGaussian(x, Mu, C)
+    x, mu, c = createVariables()
+    log_MVG = logpdf_GAU_ND(x, mu, c)
+    plot_Multivariate(log_MVG, x)
 
 
-    #print(checkResults())
-
-    #plot_Multivariate()
